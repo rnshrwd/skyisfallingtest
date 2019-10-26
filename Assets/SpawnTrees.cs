1621beb61a3155b596c1a3dbeff3ca759942af3f
rnshrwd <rnshrwd@yahoo.com>
2019-10-26 12:17:32 -0700

    adding environment
    just a plane, simple shapes and some scripts

---
+++ Assets/SpawnTrees.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrees : MonoBehaviour
+{
+    public GameObject[] trees;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+    public GameObject ground;
+
+    private int randTree;
+    void Start()
+    {
+        StartCoroutine(waitToGrow());
+    }
+
+
+    void Update()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+    }
+
+    IEnumerator waitToGrow()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (!stop)
+        {
+            randTree = Random.Range(0, 3);
+
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1, Random.Range(-spawnValues.z, spawnValues.z));
+            Instantiate(trees[randTree], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
+       
+    }
+}
